f407e87b078e2ef2ceb8f3fe83259afc939412ac
dginix <kruchininds@yandex.ru>
2023-01-24 16:36:26 +0500

    Initial server part

---
+++ server/src/main/java/com/auth_spring_react/server/ServerApplication.java
@@ -0,0 +1,13 @@
+package com.auth_spring_react.server;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ServerApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ServerApplication.class, args);
+	}
+
+}
